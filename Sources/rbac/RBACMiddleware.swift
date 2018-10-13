37c241401b27676385a0e872fa5b36bc53e40afc
Jonny <6732997+jonny7@users.noreply.github.com>
2018-10-12 22:20:21 -0300

    made rbac middlware a serive

--- Sources/rbac/RBACMiddleware.swift
+++ Sources/rbac/RBACMiddleware.swift
@@ -1,16 +1,23 @@
 import Vapor
 
-public final class RBACMiddleware: Middleware, ServiceType {
+public final class RBACMiddleware: Middleware {
     
-    public static func makeService(for worker: Container) throws -> Self {
-        return try .default(environment: worker.environment, log: worker.make())
-    }
-    
-    public func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
         // use User ID from the cache, and query DB to clarify if they have access to the route
-        return Future.map(on: request){
+        /*return Future.map(on: request){
             return Response(http: HTTPResponse(status: .ok), using: request)
+        }*/
+        // essentiall this will guard that user can perform the action on the route
+        guard 1 > 2 else {
+            throw Abort(.unauthorized, reason: "You are not authorized to perform this action")
         }
+        return try next.respond(to: request)
+        
     }
-    
+}
+
+extension RBACMiddleware: ServiceType {
+    public static func makeService(for worker: Container) throws -> RBACMiddleware {
+        return RBACMiddleware()
+    }
 }
